965368a82eafa4567480395c88edf83f7b14ef4a
amrn1991 <amrfst91@gmail.com>
2021-06-12 17:33:11 +0430

    product page design

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,45 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "./../context";
+import { ProductWrapper } from "./styles";
 
 export default class Product extends Component {
   render() {
-    return <h3>{this.props.product.id}</h3>;
+    const { id, price, title, img, inCart } = this.props.product;
+    return (
+      <ProductWrapper className="col-9 col-md-6 col-lg-3 mx-auto my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={() => console.log("clicked!")}
+          >
+            <Link to="/details">
+              <img src={img} alt="product" className="card-img-top" />
+            </Link>
+            <button
+              className="cart-btn"
+              onClick={() => console.log("added to cart!")}
+              disabled={inCart}
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  in cart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus"></i>
+              )}
+            </button>
+          </div>
+
+          <div className="card-footer d-flex justify-content-between">
+            <p className="mb-0 align-self-center">{title}</p>
+            <h5 className="text-blue fst-italic mb-0">
+              <span className="mr-1">$</span>
+              {price}
+            </h5>
+          </div>
+        </div>
+      </ProductWrapper>
+    );
   }
 }
--- src/components/styles.js
+++ src/components/styles.js
@@ -28,3 +28,50 @@ export const NavWrapper = styled.nav`
     font-size: 1.3rem;
   }
 `;
+
+export const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 0.5s linear;
+  }
+  .card-footer {
+    border-top: transparent;
+    transition: all 0.5s linear;
+  }
+  &:hover {
+    .card {
+      border: 0.04rem solid rgba(0, 0, 0, 0.2);
+      box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.2);
+    }
+    .card-footer {
+      background: rgba(220, 220, 220);
+    }
+  }
+  .card-img-top {
+    transition: all 0.5s linear;
+  }
+  .img-container {
+    position: relative;
+    overflow: hidden;
+    &:hover .card-img-top {
+      transform: scale(1.2);
+    }
+    &:hover .cart-btn {
+      transform: translate(0, 0);
+    }
+  }
+
+  .cart-btn {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0.2rem 0.4rem;
+    border: none;
+    background: var(--lightBlue);
+    color: var(--mainWhite);
+    font-size: 1.4rem;
+    border-radius: 0.5rem 0 0 0;
+    transform: translate(100%, 100%);
+    transition: all 0.5s linear;
+  }
+`;
